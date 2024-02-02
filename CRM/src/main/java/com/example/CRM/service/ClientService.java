bef5c8e906dbf3b3c8dfe6e5f5b817e31de438a2
studentsjj <marielouise04@hotmail.fr>
2024-02-02 14:25:03 +0100

    creation service

---
+++ CRM/src/main/java/com/example/CRM/service/ClientService.java
@@ -0,0 +1,33 @@
+package com.example.CRM.service;
+
+import com.example.CRM.dao.ClientRepository;
+import com.example.CRM.model.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    public void add (Client client){
+    clientRepository.save(client);
+    }
+
+    public List<Client> findAll(){
+        return clientRepository.findAll();
+    }
+    public Optional<Client> findById(Integer id){
+        return clientRepository.findById(id);
+    }
+    public void update (Client client){
+        clientRepository.save(client);
+    }
+    public void delete (Client client){
+        clientRepository.delete(client);
+    }
+}
---
+++ CRM/src/main/java/com/example/CRM/service/OrderService.java
@@ -0,0 +1,36 @@
+package com.example.CRM.service;
+
+import com.example.CRM.dao.OrderRepository;
+import com.example.CRM.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public void add (Order order){
+        orderRepository.save(order);
+    }
+    public List<Order> findAll(){
+       return orderRepository.findAll();
+    }
+
+    public Optional<Order> findById(Integer id){
+        return orderRepository.findById(id);
+    }
+
+    public void update (Order order){
+        orderRepository.save(order);
+    }
+
+    public void delete (Order order){
+        orderRepository.delete(order);
+    }
+
+}
